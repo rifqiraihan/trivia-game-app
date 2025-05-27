343451e6b2c2a55fc32c4b7970f450a47701ef5c
rifqiraihan <lazuardiraihan08@gmail.com>
2025-05-27 12:37:14 +0700

    initial commit

---
+++ trivia-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full bg-red-500 justify-center">
+        <Typography variant="h3" fontWeight="bold" mb={4} textAlign="center">
+          Trivia Game App
+        </Typography>
+      </div>
+    </>
+  );
+}
+
+export default App;
